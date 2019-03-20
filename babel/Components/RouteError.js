7572a1e04592c1df1103e5b06bf1d543b92f0233
Jeremy Shepherd <jeremyshepherd@Jeremys-MacBook-Pro.local>
2019-03-20 19:39:43 -0400

    Converted styles to CSS for RouteError

--- babel/Components/RouteError.js
+++ babel/Components/RouteError.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const styles = {
-    container: {
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'column',
-        height: '800px',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    img: {
-        width: '450px',
-        height: '450px',
-        borderRadius: '100%',
-        opacity: '0.85'
-    }
-};
-
 const RouteError = () => {
     return (
-        <div style={styles.container}>
-            <img src="../../assets/RouterError.jpeg" style={styles.img} />
-            <h1 className="header-bar">Oops! I seemed to have misplaced that route.</h1>
-            <Link className="btn btn-primary" to="/">
+        <div className="error">
+            <img src="../../assets/RouterError.jpeg" />
+            <h1 className="display">Oops! I seemed to have misplaced that route.</h1>
+            <Link className="primary-button" to="/">
                 Take me home!
             </Link>
         </div>
